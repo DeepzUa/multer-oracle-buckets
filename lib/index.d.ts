74aedd884d5bed901ff243ff7d14cba3c35c43f1
Pavel Chmykh <pavel.chmykh@gmail.com>
2022-12-07 23:43:12 +0200

    add uploadBuffer

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { Request } from 'express';
 import { StorageEngine } from 'multer';
 export interface OracleStorageConfig {
@@ -7,6 +8,7 @@ export interface OracleStorageConfig {
     fileName?: () => string;
 }
 export interface OracleFile extends Express.Multer.File {
+    memory: Buffer;
     objectName: string;
     eTag: string;
 }
@@ -16,6 +18,7 @@ export default class MulterOracleStorage implements StorageEngine {
     private getFilename;
     constructor(config: OracleStorageConfig);
     private initOracleSdk;
+    uploadBuffer(objectName: string, buffer: Buffer | null): Promise<string | null>;
     _handleFile(_req: Request, file: Express.Multer.File, cb: (error?: Error | null, file?: OracleFile) => void): Promise<void>;
     _removeFile(_req: Request, file: OracleFile, cb: (error: Error | null) => void): void;
 }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { StorageEngine } from 'multer';
 import { ConfigFileAuthenticationDetailsProvider } from 'oci-common';
 import { ObjectStorageClient} from 'oci-objectstorage';
 import { DeleteObjectResponse, PutObjectResponse } from 'oci-objectstorage/lib/response';
-import { PutObjectRequest, DeleteObjectRequest} from 'oci-objectstorage/lib/request'
+import { PutObjectRequest, DeleteObjectRequest} from 'oci-objectstorage/lib/request';
+import { streamToBuffer } from '@jorgeferrero/stream-to-buffer';
 import { v4 as uuidv4 } from 'uuid';
 
 export interface OracleStorageConfig {
@@ -14,6 +15,7 @@ export interface OracleStorageConfig {
 };
 
 export interface OracleFile extends Express.Multer.File {
+  memory: Buffer,
   objectName: string,
   eTag: string
 }
@@ -43,19 +45,44 @@ export default class MulterOracleStorage implements StorageEngine {
     this.clientOracleObjectStorage = new ObjectStorageClient({ authenticationDetailsProvider: provider });
   }
 
-  async _handleFile(_req: Request, file: Express.Multer.File, cb: (error?: Error | null, file?: OracleFile) => void) {
+  async uploadBuffer(objectName: string, buffer: Buffer | null): Promise<string | null> {
+    if (buffer === null)
+      throw new Error('buffer is null');
 
-    const objectName = this.config?.fileName  && await this.config?.fileName() || '';
     const putObjectRequest: PutObjectRequest = {
       namespaceName: this.config.namespaceName,
       bucketName: this.config.bucketName,
-      putObjectBody: file.stream,
+      putObjectBody: buffer,
+      objectName:  objectName
+    };
+
+    let uploadEtag: string | null = null;
+
+    try {
+      const uploadedObject: PutObjectResponse = await this.clientOracleObjectStorage.putObject(putObjectRequest);
+      uploadEtag = uploadedObject.eTag;
+    } catch (e) {}
+
+    return uploadEtag;
+  }
+
+  async _handleFile(_req: Request, file: Express.Multer.File, cb: (error?: Error | null, file?: OracleFile) => void) {
+
+    const objectName = this.config?.fileName  && await this.config?.fileName() || '';
+
+    const buffer = await streamToBuffer(file.stream);
+
+    const putObjectRequest: PutObjectRequest = {
+      namespaceName: this.config.namespaceName,
+      bucketName: this.config.bucketName,
+      putObjectBody: buffer,
       objectName:  objectName
     };
 
     this.clientOracleObjectStorage.putObject(putObjectRequest)
       .then( (uploadedObject: PutObjectResponse) => cb(null, {
         objectName,
+        memory: buffer,
         eTag: uploadedObject.eTag,
         ...file
       }) )
